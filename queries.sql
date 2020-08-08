6b3f721df321216b1775f1f733d6a3cedba182ae
Vahid <vahid.vaezian@gmail.com>
2020-08-08 15:58:23 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -8,12 +8,12 @@
 ---- Question Count ----
 ------------------------
 -- First we calculate question count for each programming language for each day
-SELECT DATEADD(DAY, DATEDIFF(DAY, 0, creationdate), 0) date
-  , '{}' programming_language
-  , SUM(CASE WHEN tags LIKE '%{}%' then 1 else 0 end) question_count
-FROM posts 
-WHERE posttypeid = 1
-GROUP BY DATEADD(DAY, DATEDIFF(DAY, 0, creationdate), 0)
+SELECT DATEADD(DAY, DATEDIFF(DAY, 0, CreationDate), 0) Date
+  , '{}' Programming_Language
+  , SUM(CASE WHEN Tags LIKE '%{}%' THEN 1 ELSE 0 END) Question_Count
+FROM Posts 
+WHERE PostTypeId = 1
+GROUP BY DATEADD(DAY, DATEDIFF(DAY, 0, CreationDate), 0)
 
 --Then we average over the given granularity (Yearly/Quarterly/Monthly) 
 
@@ -22,13 +22,13 @@ GROUP BY DATEADD(DAY, DATEDIFF(DAY, 0, creationdate), 0)
 ---- View Count ----
 --------------------
 -- First we calculate the normalized view count for each programming language for each day
-SELECT DATEADD(DAY, DATEDIFF(DAY, 0, creationdate), 0) date
+SELECT DATEADD(DAY, DATEDIFF(DAY, 0, CreationDate), 0) date
   , '{}' programming_language
-  , SUM(viewcount/(DATEDIFF(HOUR, creationdate, [latest_timestamp]) / 24.0)) viewcount
-FROM posts
-WHERE posttypeid = 1 
-AND tags LIKE '%{}%'
-GROUP BY DATEADD(DAY, DATEDIFF(DAY, 0, creationdate), 0)
+  , SUM(ViewCount/(DATEDIFF(HOUR, creationdate, [latest_timestamp]) / 24.0)) ViewCount
+FROM Posts
+WHERE PostTypeId = 1 
+AND Tags LIKE '%{}%'
+GROUP BY DATEADD(DAY, DATEDIFF(DAY, 0, CreationDate), 0)
 
 --Then we average over the given granularity (Yearly/Quarterly/Monthly) 
 
@@ -74,50 +74,4 @@ FROM users
 GROUP BY date
 
 --Then we average over the given granularity (Yearly/Quarterly/Monthly) 
-
-
--------------------------------------------
----- Total Distinct Users (Since 2010) ----
--------------------------------------------
--- We count the number of distinct users for each programming language since 2010
-WITH users AS (
-  -- users who asked a question with the given tag
-  SELECT DISTINCT OwnerUserId user_id
-  FROM posts
-  WHERE CreationDate >= '2010-01-01'
-    AND tags LIKE '%{}%'
-  UNION ALL
-  -- users who answered to the questions with the given tag
-  SELECT DISTINCT p2.OwnerUserId user_id
-  FROM posts p1 left join posts p2 on p1.id = p2.parentid
-  WHERE p2.CreationDate >= '2010-01-01'
-    AND p1.tags LIKE '%{}%'
-  UNION ALL
-  -- users who made an edit to the questions with the given tag
-  SELECT DISTINCT LastEditorUserId user_id
-  FROM posts
-  WHERE LastEditDate >= '2010-01-01'
-    AND tags LIKE '%{}%'
-  UNION ALL
-  -- users who made an edit to the answers of questions with the given tag
-  SELECT DISTINCT p2.LastEditorUserId user_id
-  FROM posts p1 left join posts p2 on p1.id = p2.parentid
-  WHERE p2.LastEditDate >= '2010-01-01'
-    AND p1.tags LIKE '%{}%'
-  UNION ALL
-  -- users who commented under questions with the given tag
-  SELECT DISTINCT c.userid user_id
-  FROM posts p join comments c on p.id = c.postid 
-  WHERE c.CreationDate >= '2010-01-01'
-    AND p.tags LIKE '%{}%'
-  UNION ALL
-  -- users who commented under answers to questions with the given tag
-  SELECT DISTINCT c.userid user_id
-  FROM posts p2 right join posts p1 on p1.id = p2.parentid
-                      join comments c on p2.id = c.postid 
-  WHERE c.CreationDate >= '2010-01-01'
-    AND p1.tags LIKE '%{}%'
-  )
-select COUNT(DISTINCT user_id) total_distinct_users
-from users
-
+                                                 
